4cfb6dd8d932bbbf9d6e3e7cb3419384fda412d4
Aaron Deas <deas.aaron@gmail.com>
2021-05-22 03:05:28 -0400

    Added more structure to page, added current time on top of page and added data objects for future data

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,31 @@ WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist */
+
+/*variable for saving current time
+- object for storing user's input todo and append current time
+
+*/
+
+var currentTime = document.querySelector(".current__time");
+var timeSlots = {
+	"9AM": { text: "", confirm: false },
+	"10AM": { text: "", confirm: false },
+	"11AM": { text: "", confirm: false },
+	"12AM": { text: "", confirm: false },
+	"1PM": { text: "", confirm: false },
+	"2PM": { text: "", confirm: false },
+	"3PM": { text: "", confirm: false },
+	"4PM": { text: "", confirm: false },
+	"5PM": { text: "", confirm: false },
+};
+function init() {
+	var clockDisplay = setInterval(() => {
+		var time = new Date();
+		currentTime.innerText = time;
+	}, 1000);
+}
+// console.log(time);
+// console.log("Current Time:", currentTime);
+// currentTime.innerHTML = time;
+init();
