4ffcb05398ea83a293e256b9acc287a79c514340
Aaron Deas <deas.aaron@gmail.com>
2021-05-22 04:25:08 -0400

    Added logic for colorizing hours that are avail/not avail. Interval for timer seems to be causing my page to chug

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,26 +16,46 @@ THEN the saved events persist */
 - object for storing user's input todo and append current time
 
 */
-
+var time = moment();
 var currentTime = document.querySelector(".current__time");
-var timeSlots = {
-	"9AM": { text: "", confirm: false },
-	"10AM": { text: "", confirm: false },
-	"11AM": { text: "", confirm: false },
-	"12AM": { text: "", confirm: false },
-	"1PM": { text: "", confirm: false },
-	"2PM": { text: "", confirm: false },
-	"3PM": { text: "", confirm: false },
-	"4PM": { text: "", confirm: false },
-	"5PM": { text: "", confirm: false },
-};
+var timeSlots = [
+	{
+		// Data structure for holding data of TODO App
+		"9AM": { text: "", confirm: false, hour: 9 },
+		"10AM": { text: "", confirm: false, hour: 10 },
+		"12AM": { text: "", confirm: false, hour: 11 },
+		"11AM": { text: "", confirm: false, hour: 12 },
+		"1PM": { text: "", confirm: false, hour: 13 },
+		"2PM": { text: "", confirm: false, hour: 14 },
+		"3PM": { text: "", confirm: false, hour: 15 },
+		"4PM": { text: "", confirm: false, hour: 16 },
+		"5PM": { text: "", confirm: false, hour: 17 },
+	},
+];
 function init() {
+	checkAvail();
 	var clockDisplay = setInterval(() => {
 		var time = new Date();
 		currentTime.innerText = time;
 	}, 1000);
 }
-// console.log(time);
-// console.log("Current Time:", currentTime);
-// currentTime.innerHTML = time;
+
+function checkAvail() {
+	var currentHour = time._d.getHours(); // Gets the current hour (0-24)
+	if (currentHour < 9 || currentHour > 17) {
+		// 9AM to 5PM
+		// Checks if current time is within the range of times (9am - 5pm)
+		var inputCol = document.getElementById("input__col");
+		inputColchildren = inputCol.children;
+		for (var i = 0; i < inputColchildren.length; i++) {
+			inputColchildren[i].children[0].setAttribute(
+				"class",
+				" form-control input-lg bg-danger"
+			);
+		}
+
+		console.log("Time out of bounds");
+	}
+}
+
 init();
