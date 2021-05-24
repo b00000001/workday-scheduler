323eeedf77b90d9f6546449218a9a5b0e0a3807d
Aaron Deas <deas.aaron@gmail.com>
2021-05-23 20:49:35 -0400

    Changed some logic for page, added data states to capture data from input fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,19 @@ var currentTime = document.querySelector(".current__time");
 // confirmButton.addEventListener("click", savetoStorage);
 var inputCol = document.getElementById("input__col");
 var confirmCol = document.getElementById("confirm__col");
+var currentHour = time._d.getHours(); // Gets the current hour (0-24)
 var timeSlots = [
 	{
 		// Data structure for holding data of TODO App
-		900: { text: "", confirm: false, hour: 9 },
-		1000: { text: "", confirm: false, hour: 10 },
-		1100: { text: "", confirm: false, hour: 11 },
-		1200: { text: "", confirm: false, hour: 12 },
-		1300: { text: "", confirm: false, hour: 13 },
-		1400: { text: "", confirm: false, hour: 14 },
-		1500: { text: "", confirm: false, hour: 15 },
-		1600: { text: "", confirm: false, hour: 16 },
-		1700: { text: "", confirm: false, hour: 17 },
+		900: { text: "", confirm: false, hour: 9, button: "btn0" },
+		1000: { text: "", confirm: false, hour: 10, button: "btn1" },
+		1100: { text: "", confirm: false, hour: 11, button: "btn2" },
+		1200: { text: "", confirm: false, hour: 12, button: "btn3" },
+		1300: { text: "", confirm: false, hour: 13, button: "btn4" },
+		1400: { text: "", confirm: false, hour: 14, button: "btn5" },
+		1500: { text: "", confirm: false, hour: 15, button: "btn6" },
+		1600: { text: "", confirm: false, hour: 16, button: "btn7" },
+		1700: { text: "", confirm: false, hour: 17, button: "btn8" },
 	},
 ];
 
@@ -26,28 +27,34 @@ function init() {
 		var makeButton = document.createElement("button");
 		makeButton.innerText = "Confirm";
 		makeButton.setAttribute("class", "btn btn-primary");
-		makeButton.addEventListener("click", savetoStorage);
-		console.log(makeButton);
+		makeButton.setAttribute("id", "submit__button");
+		makeButton.setAttribute("data-buttonIndex", i);
 		confirmCol.children[i].append(makeButton);
+		makeButton.addEventListener("click", function (makeButton) {
+			var buttonIndex = makeButton.originalTarget.dataset.buttonindex;
+			var inputIndex =
+				inputCol.children[makeButton.originalTarget.dataset.buttonindex]
+					.children[0].dataset.indexnumber;
+			var inputValue =
+				inputCol.children[makeButton.originalTarget.dataset.buttonindex]
+					.children[0].value;
+			savetoStorage(inputValue, inputIndex);
+			// var inputText = inputCol.children[makeButton.]
+		});
 	}
-	// add event listeners to input fields
-	for (var i = 0; i < inputCol.children.length; i++) {
-		//add event listener for every input field
-		inputCol.children[i].children[0].setAttribute("onkeydown", "getInput()");
-	}
-	var clockDisplay = setInterval(() => {
-		var time = new Date();
-		currentTime.innerText = time;
-	}, 1000);
-	currentTime.innerText = clockDisplay;
 }
 
+var clockDisplay = setInterval(() => {
+	var time = new Date();
+	currentTime.innerText = time;
+}, 1000);
+currentTime.innerText = clockDisplay;
+
 function checkAvail() {
-	var currentHour = time._d.getHours(); // Gets the current hour (0-24)
 	// Checks if current time is within the range of times (9am - 5pm)
 	if (currentHour > 9 && currentHour < 17) {
 		for (var i = 0; i < Object.keys(timeSlots[0]).length; i++) {
-			// For every item in timeslots[0]: 9 Items
+			// For every item in timeSlots[0]: 9 Items
 			switch (
 				currentHour === timeSlots[0][Object.keys(timeSlots[0])[i]].hour // Switch statement to check current hour and color chart accordingly
 			) {
@@ -86,11 +93,14 @@ function checkAvail() {
 		}
 	}
 }
-function getInput() {
-	console.log("Typing");
-}
-function savetoStorage(event) {
-	console.log(event);
+function savetoStorage(value, index) {
+	console.log("savetoStorage()", value, index);
+	timeSlots[0][Object.keys(timeSlots[0])[index]].text = value;
+	timeSlots[0][Object.keys(timeSlots[0])[index]].index = index;
+	localStorage.setItem(
+		"timeSlotsstore",
+		JSON.stringify(timeSlots[0])[Object.keys(timeSlots[0])[index]]
+	);
 }
 
 init();
