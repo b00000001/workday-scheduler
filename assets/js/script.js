d6833087e7cdb414912074d6dcd47d7fdb91a74c
Aaron Deas <deas.aaron@gmail.com>
2021-05-24 15:01:56 -0400

    Worked with instructor to fix logic on applying values to inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,18 @@ var inputCol = document.getElementById("input__col");
 var confirmCol = document.getElementById("confirm__col");
 var timeslotArray = []; // Array to hold the full array of timeslots that have been altered
 var currentHour = time._d.getHours(); // Gets the current hour (0-24)
-var timeSlots = [
-	{
-		// Data structure for holding data of TODO App
-		900: { hour: "9", buttonId: "btn0" },
-		1000: { hour: "10", buttonId: "btn1" },
-		1100: { hour: "11", buttonId: "btn2" },
-		1200: { hour: "12", buttonId: "btn3" },
-		1300: { hour: "13", buttonId: "btn4" },
-		1400: { hour: "14", buttonId: "btn5" },
-		1500: { hour: "15", buttonId: "btn6" },
-		1600: { hour: "16", buttonId: "btn7" },
-		1700: { hour: "17", buttonId: "btn8" },
-	},
-];
+var timeSlots = JSON.parse(localStorage.getItem("timeSlots")) || {
+	// Data structure for holding data of TODO App
+	9: "",
+	10: "",
+	11: "",
+	12: "",
+	13: "",
+	14: "",
+	15: "",
+	16: "",
+	17: "",
+};
 
 function init() {
 	checkAvail();
@@ -46,21 +44,13 @@ function init() {
 
 	/* 
     ----
-    This section checks if the local storage timeSlotsstore Array exists and if it does then
-    A message is logged to the console and for each object entry in storageData object
-    a check is run to verify if the object at the position of the iterator has a 'text' field
-    if so, the corresponding input value is retrieved from storage.
+    
     ----
     */
-	if (window.localStorage.timeSlotsstore) {
-		storageData = JSON.parse(window.localStorage.timeSlotsstore);
-		console.log("Data in local store", storageData);
-		for (var i = 0; i < Object.keys(storageData).length; i++) {
-			if (storageData[Object.keys(storageData)[i]].text) {
-				inputCol.children[i].children[0].value =
-					storageData[Object.keys(storageData)[0]].text;
-			}
-		}
+
+	for (var i = 0; i < Object.keys(timeSlots).length; i++) {
+		inputCol.children[i].children[0].value =
+			timeSlots[Object.keys(timeSlots)[i]];
 	}
 }
 
@@ -80,15 +70,13 @@ Future: Green
 function checkAvail() {
 	// Checks if current time is within the range of times (9am - 5pm)
 	if (currentHour >= 9 && currentHour <= 17) {
-		console.log("office hours");
-		for (var i = 0; i < Object.keys(timeSlots[0]).length; i++) {
-			// For every item in timeSlots[0]: 9 Items
-			switch (
-				currentHour ===
-				parseInt(timeSlots[0][Object.keys(timeSlots[0])[i]].hour) // Switch statement to check current hour and color chart accordingly
-			) {
+		console.log("Currently within office hours");
+		for (var i = 0; i < Object.keys(timeSlots).length; i++) {
+			// For every item in timeSlots: 9 Items
+
+			switch (currentHour === parseInt(Object.keys(timeSlots)[i])) {
+				// Switch statement to check current hour and color chart accordingly
 				case true:
-					console.log("current hour");
 					var inputColchildren = inputCol.children;
 					inputColchildren[i].children[0].setAttribute(
 						"class",
@@ -96,7 +84,7 @@ function checkAvail() {
 					);
 					break;
 				case false:
-					if (currentHour > timeSlots[0][Object.keys(timeSlots[0])[i]].hour) {
+					if (currentHour > parseInt(Object.keys(timeSlots)[i])) {
 						var inputColchildren = inputCol.children;
 						inputColchildren[i].children[0].setAttribute(
 							"class",
@@ -119,8 +107,8 @@ function checkAvail() {
 
         --------
         */
-		for (var i = 0; i < Object.keys(timeSlots[0]).length; i++) {
-			// For every item in timeslots[0]: 9 Items
+		for (var i = 0; i < Object.keys(timeSlots).length; i++) {
+			// For every item in timeSlots: 9 Items
 			var inputColchildren = inputCol.children;
 			inputColchildren[i].children[0].setAttribute(
 				"class",
@@ -132,12 +120,11 @@ function checkAvail() {
 
 function savetoStorage(value, index) {
 	console.log("savetoStorage()", value, index);
-	timeSlots[0][Object.keys(timeSlots[0])[index]].text = value;
-	timeSlots[0][Object.keys(timeSlots[0])[index]].index = index;
+	timeSlots[Object.keys(timeSlots)[index]] = value;
 
-	timeslotArray.push(timeSlots[0][Object.keys(timeSlots[0])[index]]);
+	// timeslotArray.push(timeSlots[Object.keys(timeSlots)[index]]);
 
-	localStorage.setItem("timeSlotsstore", JSON.stringify(timeslotArray));
+	localStorage.setItem("timeSlots", JSON.stringify(timeSlots));
 }
 
 init();
